cd0484c7f5573467c5c08fb09d4b68aa80a8c157
root <root@localhost.localdomain>
2015-05-26 22:50:28 -0700

    Novos arquivos: .screenrc -> Configuracoes do screen .vimrc    -> Configuracoes do vim
    e Install melhorado

--- install.sh
+++ install.sh
@@ -2,3 +2,15 @@
 
 
 echo #Instador para o Shell Enviroment KRN"
+
+
+echo -e "\n\n Iniciando a instalacao ...\n\n"
+
+
+cp .alias $HOME/
+cp .export $HOME/
+cp .vimrc $HOME/
+cp .screenrc $HOME/
+
+
+echo " Arquivos instalados com sucesso\n"
