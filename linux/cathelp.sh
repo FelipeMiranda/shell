25c6608eeb89ea456ce0c5e1c8d81b4a7ce9d892
Rafael Quirino <quirinobytes@gmail.com>
2015-05-30 01:19:39 -0300

    Mostra o arquivo do help do determinado Sistema Operacional

---
+++ linux/cathelp.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/ksh
+
+
+#Exibe os arquivos do help Miniman
+
+
+VAR=$1
+
+if [ -z $VAR ] ; then
+     echo "Digite algum nome de programa para visualizar o help"
+else
+	if [ -e ~/help/$VAR.hlp ]; then
+        	cat ~/help/$VAR.hlp
+        else
+                echo "Programa nao encontrado no diretório ~/help/"
+	fi
+fi
+
