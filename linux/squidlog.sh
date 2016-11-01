cc64097f6e184c3ec626d6ace2dae16de59b0968
Rafael <quirinobytes@gmail.com>
2016-11-01 19:39:48 -0200

    Melhorias no squidlog.sh para funcionar com cores nas colunas, identificando os usuários mais facilmente, permitindo o fácil reconhecimento das URLs e o IP da maquina de origem.

--- linux/squidlog.sh
+++ linux/squidlog.sh
@@ -1,1 +1,17 @@
-tail -s 0.5 -f /var/log/squid/access.log | awk '{ printf strftime("%H:%M %d/%m ") } $0'
+#!/bin/bash
+tail -n 12 -s 0.5 -f /var/log/squid/access.log | awk '{ printf strftime("%H:%M %d/%m ") } $0' | awk '
+
+	
+
+	$3   ~ 'DENIED' {printf "\033[1;31m"$15"\033[1;30m\t"$10"\t\033[1;38m"$11"\t"$12"\t\033[1;32m"$13"\t\033[1;34m %s \n",$14}
+	$3 !~ 'DENIED' {printf "\033[1;31m"$15"\033[1;30m\t"$10"\t"$11"\t"$12"\t\033[1;32m"$13"\t\033[1;34m %s \n",$14}
+   
+	
+	
+	
+	'
+
+
+
+
+	#$1!='mariana' printf "\033[1;31m"$15"\033[1;30m\t"$10"\t"$11"\t"$12"\t\033[1;32m"$13"\t\033[1;34m"$14"\n"
