1e41cb0db3087a543fd89e6deadda37c294ce197
root <root@zaroio.localdomain>
2016-12-29 18:23:54 -0200

    Melhoria da instalação dos pacotes do cdshell para instalar o roff tmb junto com o requisitos-INSTALL.sh

--- requisitos-INSTALL.sh
+++ requisitos-INSTALL.sh
@@ -6,17 +6,27 @@ echo -e "Instalando os requisitos ... \n"
 OSTYPE=`uname`
 
 
+############ Linux ##############################
 if [ $OSTYPE == "Linux" ]; then
 
+	#instalar o rubydevel para funcionar o ronn
+	yum -y install gcc ruby-devel rubygems
+	
+	#instalar o screen e outras dependencias COMUNS, tudo que deveria ser de praxe
+	yum install screen vim lynx iftop ifstatus -y
 
-	yum install screen vim lynx iftop ifstatus ronn
+	#instalar o ronn para fazer os miniman
+	gem install ronn
 
 	#CentOS 6.6
    rpm -hiv http://pkgs.repoforge.org/txt2tags/txt2tags-2.6-1.el6.rf.noarch.rpm
 
+
+
 fi
 
 
+############ OpenBSD ##############################
 if [ $OSTYPE == "OpenBSD" ]; then
 #LISTA de requisitos para ser instalados no OpenBSD
 LISTA="git vim-7.4.475-no_x11-ruby lynx iftop ngrep bash colorls wget autoconf-2.69p1 automake-1.9.6p11"
@@ -33,7 +43,7 @@ echo -e "Se desejar alterar o shell do usuário:\n usermod -s /usr/local/bin/bas
 fi
 
 
-
+############ FreeBSD ##############################
 if [ $OSTYPE == "FreeBSD" ]; then
 #LISTA de requisitos para ser instalados no FreeBSD
 LISTA="git vim lynx iftop ngrep bash colorls wget autoconf automake binutils"
