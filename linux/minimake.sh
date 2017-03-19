a7a9a094f673626cf77a12d2a0720f254b4c2c96
Rafael <quirinobytes@gmail.com>
2017-03-19 07:06:02 -0300

    Resolvendo um bug no minimake

--- linux/minimake.sh
+++ linux/minimake.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 
+
 if [ ! -z $1 ] ; then
 MINIMAN_FILENAME=$1.md
+MINIMAN_NAME=$1
 else
 	echo "Usar $$ novo_arquivo_miniman"
-	exit 0
+	exit 1
 fi
 
 
 cd /$USER/help/Linux/help
 cp model.md $MINIMAN_FILENAME
-sed -i "s/model/$MINIMAN_FILENAME/g" $MINIMAN_FILENAME
+sed -i "s/model/$MINIMAN_NAME/g" $MINIMAN_FILENAME
 cd -
-
-
-
