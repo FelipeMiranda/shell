dbf8dc10b299c3a087e3f7176d72395da1634e67
Rafael Castro do UOL <quirinobytes@gmail.com>
2017-03-23 18:14:45 -0300

    Novo arquivo para sftpe

---
+++ linux/senhaback.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+if [ ! -z $1 ] ; then
+
+	HOST=$1
+	echo Trocando a senha no servidor: $HOST
+	/root/uol/senhas/troca_senha_expirada.exp $HOST > /dev/null
+	/root/uol/senhas/destroca_para_Bi.exp $HOST > /dev/null
+
+else
+
+	echo Para trocar sua senha para a padrao no servidor_X.
+	echo Use $0 servidor_X
+
+fi
