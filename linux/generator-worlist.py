ef7030532dbaee1c0cc6854ae688e3c9202814e5
Rafael <quirinobytes@gmail.com>
2017-05-07 21:18:10 -0300

    Melhorias

--- linux/generator-worlist.py
+++ linux/generator-worlist.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3.4
 import itertools
 import string
+import sys
 
-hex_chars = '0123456789abcdef'
 
-wordlist = open('wordlist', 'a')
+def main():
+   if sys.argv[1]:
+	   caracteres = sys.argv[1]
+   else:
+	   caracteres = 5
 
-for words in itertools.product(hex_chars, repeat=5):
-	wordlist.write(''.join(words) + 'shx' + '\n')
+   if len(sys.argv) > 2:
+	   if sys.argv[2]!= NULL :
+		   hex_chars = sys.argv[2]
+   else:
+	   hex_chars = '0123456789abcdef'
+
+   wordlist = open('wordlist', 'w')
+
+   for words in itertools.product(hex_chars, repeat=int(caracteres)):
+	   wordlist.write('shx' +''.join(words) + '' + '\n')
+
+
+if __name__ == '__main__':
+  main()
