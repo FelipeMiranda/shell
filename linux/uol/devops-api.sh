315d0db8f4fbba0a1f87f29ea5d299f5744120ee
Rafael Castro do UOL <quirinobytes@gmail.com>
2018-02-01 20:26:34 -0200

    Novo alias puppet2

--- linux/uol/devops-api.sh
+++ linux/uol/devops-api.sh
@@ -6,7 +6,7 @@ cat > /tmp/1
 
 cat /tmp/1 | grep -v uol | sed '/^$/d' | awk '/@/{print $NF}' > /tmp/afil.list
 
-#cat /tmp/1 | grep -v dominiotemporario | sed '/^$/d' > /tmp/uol.list
+cat /tmp/1 | grep -v dominiotemporario | grep -v bol.com.br | grep -v devops | sed '/^$/d' > /tmp/uol.list
 
 
 for EMAIL in $(cat /tmp/afil.list); do
