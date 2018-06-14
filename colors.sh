3adb1a5d187bfd2af36f3b669b27272595bb23d3
Rafael @ mail <quirinobytes@gmail.com>
2018-06-14 06:13:17 -0300

    cores animadas para o shell

--- colors.sh
+++ colors.sh
@@ -1,20 +1,21 @@
-#Variáveis para usar no ENVIROMENT
-#			   .	       ;
-			   #__CORES__#
-			   red='\e[31m'
-			  black='\e[30m'
-			   cyan='\e[36m'
-			      blue='\e[34m'
-			     white='\e[37m'
-			     magenta='\e[35m'
-			       green='\e[32m'
-			  			 yellow='\e[33m'
-			         WHITE='\e[40;37;1m'
-			        #CRIATIVIDADE   AQUI'
-			       atention='\e[41;39m'
-			      alert='\e[44;33;1m'
-					 normal='\e[0;0m'
-				     #esp#     \#aow'
-				      #pa#=     \r#h'
-				        #mia     \o_#
-				      #g#BW^j    .gW;^j
+#Variáveis para encapetar seu ENVIROMENT#
+#			     .      ;                  #
+		 	     #BCORES(#                 #
+			    red='\e[31m'               #
+			   black='\e[30m'              #
+			    cyan='\e[36m'              #
+			      blue='\e[34m'            #
+			      white='\e[37m'           #
+			     magenta='\e[35m'          #
+			         green='\e[32m'        #
+                     yellow='\e[33m'    #
+			          WHITE='\e[40;37;1m'  #
+			        #/CRIATIVADE\o/AQUI/   #
+			      atention='\e[41;39m'     #
+			      alert='\e[44;33;1m'      #
+					 normal='\e[0;0m'        #
+                  #ep@\   \#ow'         #
+                   #pa#=    \r#h'       #
+                     #mia     \o_#      #
+#                 #g#iBW^j   .ozWB^j    #
+#########################################						
