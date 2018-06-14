6fa9d0e4483010ae6e9272154f9dd2afc4a640e7
Rafael @ mail <quirinobytes@gmail.com>
2018-06-14 05:47:46 -0300

    Novo colors
    Encapetando as cores do linux.

--- colors.sh
+++ colors.sh
@@ -1,16 +1,20 @@
-#Meu arquivo de cores para usar no ENVIROMENT
-
-black='\e[30m'
-red='\e[31m'
-green='\e[32m'
-yellow='\e[33m'
-blue='\e[34m'
-magenta='\e[35m'
-cyan='\e[36m'
-white='\e[37m'
-WHITE='\e[40;37;1m'
-
-alert='\e[44;33;1m'
-atention='\e[41;39m'
-normal='\e[0;32m'
-normal='\e[0;0m'
+#Variáveis para usar no ENVIROMENT
+#			   .	       ;
+			   #__CORES__#
+			   red='\e[31m'
+			  black='\e[30m'
+			   cyan='\e[36m'
+			      blue='\e[34m'
+			     white='\e[37m'
+			     magenta='\e[35m'
+			       green='\e[32m'
+			  			 yellow='\e[33m'
+			         WHITE='\e[40;37;1m'
+			        #CRIATIVIDADE   AQUI'
+			       atention='\e[41;39m'
+			      alert='\e[44;33;1m'
+					 normal='\e[0;0m'
+				     #esp#     \#aow'
+				      #pa#=     \r#h'
+				        #mia     \o_#
+				      #g#BW^j    .gW;^j
