4a142180bb6c343950dd6835fe61d64daeb3bb8f
Rafael @ mail <quirinobytes@gmail.com>
2018-06-14 06:25:16 -0300

    Trabalho de customizacao da colors.sh finalizado.

--- colors.sh
+++ colors.sh
@@ -1,19 +1,19 @@
 #Variáveis para encapetar seu ENVIROMENT#
-#			     .      ;                  #
-		 	     #BCORES(#                 #
-			    red='\e[31m'               #
-			   black='\e[30m'              #
-			    cyan='\e[36m'              #
-			      blue='\e[34m'            #
-			      white='\e[37m'           #
-			     magenta='\e[35m'          #
-			         green='\e[32m'        #
+#             .      ;                  #
+              #BCORES(#                 #
+              red='\e[31m'              #
+             black='\e[30m'             #
+             cyan='\e[36m'              #
+               blue='\e[34m'            #
+               white='\e[37m'           #
+              magenta='\e[35m'          #
+                  green='\e[32m'        #
                      yellow='\e[33m'    #
-			          WHITE='\e[40;37;1m'  #
-			        #/CRIATIVADE\o/AQUI/   #
-			      atention='\e[41;39m'     #
-			      alert='\e[44;33;1m'      #
-					 normal='\e[0;0m'        #
+                   WHITE='\e[40;37;1m'  #
+                 #/CRIATIVADE\o/AQUI/   #
+               atention='\e[41;39m'     #
+               alert='\e[44;33;1m'      #
+                normal='\e[0;0m'        #
                   #ep@\   \#ow'         #
                    #pa#=    \r#h'       #
                      #mia     \o_#      #
