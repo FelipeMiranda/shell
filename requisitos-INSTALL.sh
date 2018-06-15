7765162072280537f11b4dd222893fb0d70e5cd5
Rafael @ mail <quirinobytes@gmail.com>
2018-06-15 06:15:03 -0300

    Melhorando o requisitos Install

--- requisitos-INSTALL.sh
+++ requisitos-INSTALL.sh
@@ -1,75 +1,53 @@
 #!/bin/bash
 
 
-echo -e "Instalando os requisitos ... \n"
+echo -en "\n Instalando os requisitos .............."
 
 OSTYPE=`uname`
 
-
 ############ Linux ##############################
 if [ $OSTYPE == "Linux" ]; then
+	 PROGRAMAS_YUM="colordiff gcc ruby-devel rubygems screen vim lynx iftop ifstatus ronn htop"
+	 echo -en "\n Vamos instalar esses pacotes: $green $PROGRAMAS_YUM $normal\n"
+	 echo -en "\n\n\t Pressione qualquer tecla para continuar..."
+	 read -n 1
 
-	#instalar o colordiff
-	yum install colordiff -y
-
-	#instalar o rubydevel para funcionar o ronn
-	yum -y install gcc ruby-devel rubygems
-	
-	#instalar o screen e outras dependencias COMUNS, tudo que deveria ser de praxe
-	yum install screen vim lynx iftop ifstatus -y
-
-	#instalar o ronn para fazer os miniman
-	gem install ronn
+	 for programa in $PROGRAMAS_YUM ; do
+		 echo -en "\n Instalando o$yellow $programa \n"
+		 yum install $programa -y
+	 done
 
 	#CentOS 6.6
    rpm -hiv http://pkgs.repoforge.org/txt2tags/txt2tags-2.6-1.el6.rf.noarch.rpm
-
-	#Htop
-	yum install htop
-
 fi
-
-
-PROGRAMAS_YUM="jq"
-
-for programa in $PROGRAMAS_YUM; do
-
-	echo -en "\n Instalando o$yellow $PRORGAMA_YUM"
-	read -n 1
-	exit
-done
-
 
 ############ OpenBSD ##############################
 if [ $OSTYPE == "OpenBSD" ]; then
-#LISTA de requisitos para ser instalados no OpenBSD
-LISTA="git vim-7.4.475-no_x11-ruby lynx iftop ngrep bash colorls wget autoconf-2.69p1 automake-1.9.6p11"
+	#LISTA de requisitos para ser instalados no OpenBSD
+	LISTA="git vim-7.4.475-no_x11-ruby lynx iftop ngrep bash colorls wget autoconf-2.69p1 automake-1.9.6p11"
 
-for i in $LISTA; do
-pkg_add $i 
-done 
+	for i in $LISTA; do
+		 pkg_add $i 
+	done 
 
-. $CDSHELL/openbsd/install-screen.sh
+	. $CDSHELL/openbsd/install-screen.sh
 
-#mudar o shell do usuário para bash
-echo -e "Se desejar alterar o shell do usuário:\n usermod -s /usr/local/bin/bash $USER"
-
+	#mudar o shell do usuário para bash
+	echo -e "Se desejar alterar o shell do usuário:\n usermod -s /usr/local/bin/bash $USER"
 fi
-
 
 ############ FreeBSD ##############################
 if [ $OSTYPE == "FreeBSD" ]; then
-#LISTA de requisitos para ser instalados no FreeBSD
-LISTA="git vim lynx iftop ngrep bash colorls wget autoconf automake binutils"
+	#LISTA de requisitos para ser instalados no FreeBSD
+	LISTA="git vim lynx iftop ngrep bash colorls wget autoconf automake binutils"
 
-for i in $LISTA; do
-pkg install $i
-done
-echo tentando instalar o screen no FreeBSD
-. $CDSHELL/openbsd/install-screen.sh
+	for i in $LISTA; do
+	pkg install $i
+	done
+	echo tentando instalar o screen no FreeBSD
+	. $CDSHELL/openbsd/install-screen.sh
 
-#mudar o shell do usuário para bash
-echo -e "Se desejar alterar o shell do usuário:\n usermod -s /usr/local/bin/bash $USER"
+	#mudar o shell do usuário para bash
+	echo -e "Se desejar alterar o shell do usuário:\n usermod -s /usr/local/bin/bash $USER"
 
 fi
-
