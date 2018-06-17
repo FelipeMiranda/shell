c3513d165210b1f671b019e9354550fa536a08c8
Rafael @ mail <quirinobytes@gmail.com>
2018-06-17 18:52:55 -0300

    Novo script de docker.

---
+++ linux/dk.sh
@@ -0,0 +1,51 @@
+
+#!/bin/bash
+
+#################################################
+# Docker script						#
+# Script para automatizar execução de comandos. #
+# v_1.0.0							#
+#################################################
+
+
+#############     CONFIG     ####################
+#Carrega variáreis de ambiente.
+source ~/.export
+# Suporte a cores no bash.
+source ~/shell/colors.sh
+################################################
+
+
+##########  Funcao       #######################
+function versao(){
+
+}
+
+
+################################################
+#############        MAIN       ################
+case $1 in
+		"ps" | "-p" | "--ps")	
+			# Faça isso... 
+			docker ps
+		;;
+
+
+		"-k"|"--kill" )	
+			# Quando executa sem opcao, chama funcao versao acima.
+			IDS=$( dk ps | grep vendedor | cut -f1 -d " ")
+			for ID in $IDS; do 
+			    echo matando $ID
+			    echo $ID
+			done
+		;;
+
+	
+		* )
+			# Executa com opcao que nao tem.
+			echo -en "O comando $red $1 $normal não existe.\n"
+		;;
+esac
+#############        FIM      ##################
+################################################
+
