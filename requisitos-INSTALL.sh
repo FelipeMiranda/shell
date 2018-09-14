a2fe7bf5dc246202bcfdd55c648fc195a36f4536
Rafael <quirinobytes@gmail.com>
2018-09-14 13:43:42 -0300

    choco install wget para windows OSTYPE

--- requisitos-INSTALL.sh
+++ requisitos-INSTALL.sh
@@ -92,6 +92,8 @@ if [ $OSTYPE == "Windows" ]; then
 
 runas /savecred /user:Administrator "/c/WINDOWS/system32/cmd.exe" -k @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
+choco install wget
+
 fi
 
 # Chegou aqui, significa que executou tudo, entao cria o arquivo de sinalizacao da primeira instalacao executada.
