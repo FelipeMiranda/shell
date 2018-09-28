88d975d7593b14a3a5dd1312eb26c6b785fdca62
Rafael <quirinobytes@gmail.com>
2018-09-27 22:15:04 -0300

    OK

--- linux/notificar.sh
+++ linux/notificar.sh
@@ -6,15 +6,11 @@ while(true); do
     sleep 1;
     pgrep install.sh > /dev/null
     if [ $? -ne 0 ]; then
-#		echo -en "\033\0143" > $OUTPUT
+		echo -en "\033\0143" > $OUTPUT
 		echo -en "\n\n$alert Instalaçao CDSHELL $normal  ->  $green DONE $normal \n\n" > $OUTPUT
-#		screen -p 1 -t teste /root/shell/linux/cdshell -t
-#		screen -X exec /root/shell/linux/cdshell -a
 		screen -t instalado -X split -v
 		screen -p instalado -X focus
 		screen -X screen -t teste -p 2 /root/shell/linux/-t
-#		screen -p instalado -X focus
-#		screen -p teste -X kill
 		exit 0
     fi
 done
