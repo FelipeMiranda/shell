6b3cc9069514298bf92e36fdb8c1c278797a602a
Rafael <quirinobytes@gmail.com>
2018-09-29 03:19:21 -0300

    OK

--- requisitos-INSTALL.sh
+++ requisitos-INSTALL.sh
@@ -3,7 +3,7 @@ source ~/colors.sh
 source ~/.export
 
 OSTYPE=`uname`
-PACOTES_LINUX="colordiff gcc gcc-dev ruby-devel rubygems screen vim lynx iftop ifstatus ronn htop jq net-tools htop dialog python34 nodejs docker netcat tcpdump nmap tcl puppet ansible dig mc lsof bc bind-utils"
+PACOTES_LINUX="colordiff gcc gcc-dev ruby-devel rubygems screen vim lynx iftop ifstatus ronn htop jq net-tools htop dialog python34 nodejs docker netcat tcpdump nmap tcl puppet ansible dig mc lsof bc bind-utils tmux "
 PACOTES_OPENBSD="git vim-7.4.475-no_x11-ruby lynx iftop ngrep bash colorls wget autoconf-2.69p1 automake-1.9.6p11"
 PACOTES_FREEBSD="git vim lynx iftop ngrep bash colorls wget autoconf automake binutils"
 
