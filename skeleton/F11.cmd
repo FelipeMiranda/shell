7b453b6786b09af89ae58cb8ef30e4c676f7eaa5
Rafael <quirinobytes@gmail.com>
2018-09-30 02:16:49 -0300

    OK

--- skeleton/F11.cmd
+++ skeleton/F11.cmd
@@ -1,1 +1,1 @@
-echo -en '\n\n$green\n\t\t <COLOQUE SEU COMANDO AQUI> $normal\n\n';read -n 1
+echo -en '\n\n$green\n\t\t Digite \$> F11 "echo Hello World" <ENTER> e depois com scr rodando -> F11 $normal\n\n'  ; read -n 1
