778d071d1a1b07d58fed6300b6f1d56b2cd0913f
Rafael <quirinobytes@gmail.com>
2018-10-15 01:57:58 -0300

    OK

--- push/deploy.js
+++ push/deploy.js
@@ -5,14 +5,14 @@
 	var socket = io.connect('http://servidorpush.superati.com.br:3000')
 
 	//buttons and inputs
-	var message = "version:" + process.argv[2]
+	var message = process.argv[2]
 	var host = os.hostname();
 
 	//Emit a username
 		socket.emit('username', {username : host }) 
 
 	//Emit message
-		socket.emit('message', {message : 'deploy' })
+		socket.emit('message', {message : message })
 		socket.emit('sair', {message : "sair" })
 		
 		socket.on("sair", (data) => {
