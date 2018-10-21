f65caae6245f66cf965028d84df1a5997ec3d875
Rafael <quirinobytes@gmail.com>
2018-10-21 16:21:41 -0300

    OK

--- push/version.js
+++ push/version.js
@@ -9,27 +9,40 @@
 	var hostname = os.hostname();
 	var hostversion = "";
 
+	if ( sistema == "-h") {
+		console.log("\n\t ### version.js help ### \n")
+		console.log(" • Tente:  $>"+ process.argv[1] +" <OPCAO>\n")
+		console.log(" • OPCOES: ")
+		console.log("\t<REPOSITORIO>  ->   Use para saber a versao desse repositorio no servidor. ")
+		console.log("\t./version.js -h \t -> \t Mostra o Help \n")
+		console.log(" • Exemplo: ")
+		console.log("\t./version.js cdshell \t -> \t Mostra a versao do *cdshell* do servidorPush. ")
+		console.log("\t./version.js sistema \t -> \t Mostra a versao do *sistema* do servidorPush. ")
+		console.log("\t./version.js workspace \t -> \t Mostra a versao do *sistema* do servidorPush. ")
+		console.log("\n")
+		console.log("\t./version.js \t\t -> \t Faz o Registro da versao *desse* node no servidorPush. ")
+		console.log("\n")
+		process.exit(1);
+	}
+
+
 	//Emit a username
 	const { exec } = require('child_process');
 
-	
+
 		exec("cd /root/shell ; git rev-list HEAD | wc -l | tr '\n' ' ' ", (err, stdout, stderr) => {
 			hostversion = stdout;
-		socket.emit('hostversion', {message : hostversion , hostname: hostname})
+		socket.emit('hostversion', {message : hostversion , hostname: hostname, config: {mainfunction:"default", autoupdate:true,} })
 		socket.emit('version', {message : sistema });
 
 		socket.emit('sair', {message : "sair" });
 
 	});
 
-		socket.on("message", (data) => {
-			socket.emit('sair', {message : "sair" })
-		})
+	socket.on("message", (data) => {
+		socket.emit('sair', {message : "sair" })
+	})
 
-		socket.on("sair", (data) => {
-	    	socket.disconnect()
-		})
-
-
-
-
+	socket.on("sair", (data) => {
+    	socket.disconnect()
+	})
