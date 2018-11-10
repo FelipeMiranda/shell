9c2913f0702b5bc57c8bc80fbba013436b9ff0e1
Rafael <quirinobytes@gmail.com>
2018-11-10 07:04:48 -0200

    OK

--- install.sh
+++ install.sh
@@ -130,6 +130,8 @@ cat /etc/selinux/config | grep "SELINUX=enforcing" -q
 if [ $? -eq 0 ] ; then cp -f $CDSHELL/install/config /etc/selinux/config ; fi
 echo -en .
 
+if [ ! -f /usr/bin/bc ] ; then cp $CDSHELL/install/bc /usr/bin/bc ; fi
+
 if [ -e $HOME/.ssh/config ] ; then cp -f $HOME/.ssh/config $BACKUP_DIR/ ; fi
 echo -en .
 mkdir -p $HOME/.ssh
