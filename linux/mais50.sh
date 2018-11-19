c16baa973a3171a812bd6e76741ddefb23da2311
Rafael <quirinobytes@gmail.com>
2018-11-19 08:47:30 -0200

    OK

---
+++ linux/mais50.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curl -i -H 'X-API-Project-Id: 1a5' -H 'Accept: application/json'  -H 'Content-Type: application/json' -X POST 'http://192.168.15.25:8080/v1-webhooks/endpoint?key=p70U3iXfNPVJa49tsfzc8Ixpqg9px1j8TqANmJ7a&projectId=1a5'
+curl -i -H 'X-API-Project-Id: 1a5' -H 'Accept: application/json'  -H 'Content-Type: application/json' -X POST 'http://192.168.15.25:8080/v1-webhooks/endpoint?key=p70U3iXfNPVJa49tsfzc8Ixpqg9px1j8TqANmJ7a&projectId=1a5'
+curl -i -H 'X-API-Project-Id: 1a5' -H 'Accept: application/json'  -H 'Content-Type: application/json' -X POST 'http://192.168.15.25:8080/v1-webhooks/endpoint?key=p70U3iXfNPVJa49tsfzc8Ixpqg9px1j8TqANmJ7a&projectId=1a5'
+curl -i -H 'X-API-Project-Id: 1a5' -H 'Accept: application/json'  -H 'Content-Type: application/json' -X POST 'http://192.168.15.25:8080/v1-webhooks/endpoint?key=p70U3iXfNPVJa49tsfzc8Ixpqg9px1j8TqANmJ7a&projectId=1a5'
+curl -i -H 'X-API-Project-Id: 1a5' -H 'Accept: application/json'  -H 'Content-Type: application/json' -X POST 'http://192.168.15.25:8080/v1-webhooks/endpoint?key=p70U3iXfNPVJa49tsfzc8Ixpqg9px1j8TqANmJ7a&projectId=1a5'
+curl -i -H 'X-API-Project-Id: 1a5' -H 'Accept: application/json'  -H 'Content-Type: application/json' -X POST 'http://192.168.15.25:8080/v1-webhooks/endpoint?key=p70U3iXfNPVJa49tsfzc8Ixpqg9px1j8TqANmJ7a&projectId=1a5'
+curl -i -H 'X-API-Project-Id: 1a5' -H 'Accept: application/json'  -H 'Content-Type: application/json' -X POST 'http://192.168.15.25:8080/v1-webhooks/endpoint?key=p70U3iXfNPVJa49tsfzc8Ixpqg9px1j8TqANmJ7a&projectId=1a5'
+curl -i -H 'X-API-Project-Id: 1a5' -H 'Accept: application/json'  -H 'Content-Type: application/json' -X POST 'http://192.168.15.25:8080/v1-webhooks/endpoint?key=p70U3iXfNPVJa49tsfzc8Ixpqg9px1j8TqANmJ7a&projectId=1a5'
+curl -i -H 'X-API-Project-Id: 1a5' -H 'Accept: application/json'  -H 'Content-Type: application/json' -X POST 'http://192.168.15.25:8080/v1-webhooks/endpoint?key=p70U3iXfNPVJa49tsfzc8Ixpqg9px1j8TqANmJ7a&projectId=1a5'
