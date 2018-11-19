aaf41b840062daa877593761e1fc41e9ed32f065
Rafael <quirinobytes@gmail.com>
2018-11-19 09:02:40 -0200

    OK

---
+++ linux/menos5.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -i -H 'X-API-Project-Id: 1a5' -H 'Accept: application/json'  -H 'Content-Type: application/json' -X POST 'http://192.168.15.25:8080/v1-webhooks/endpoint?key=Tmt67nqIEKdabp0OVrUj7b7EXXGlZPFG2gbnR44I&projectId=1a5'
+
