6756b03c178c8362589640e45b68f9799cdeb9ac
Rafael <quirinobytes@gmail.com>
2018-11-24 23:29:08 -0200

    OK

--- linux/install-nodejs8.sh
+++ linux/install-nodejs8.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
-rm nodesource_setup.sh
+curl -fsSL https://raw.githubusercontent.com/nodesource/distributions/master/deb/setup_6.x | bash -
+apt install nodejs -y
 
