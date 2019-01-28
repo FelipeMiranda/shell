4cd11ef60f9fe42b9492886a6a8e5854227e9079
Rafael <quirinobytes@gmail.com>
2019-01-28 14:40:24 -0200

    OK

--- linux/kcontext.sh
+++ linux/kcontext.sh
@@ -23,7 +23,7 @@ case $1 in
 
 		"" )	
 			# Quando executa sem opcao, chama funcao versao acima.
-			kubectl config get-contexts | realce '*'
+			kubectl config get-contexts | realce.awk '*'
 		;;
 
 		* )
