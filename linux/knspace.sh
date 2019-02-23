69f78be000939ffc34f65ed87fae53a14390ded2
Rafael <rafael.castro@btgpactual.com>
2019-02-23 10:42:11 -0300

    OK

--- linux/knspace.sh
+++ linux/knspace.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+####!/bin/bash
 
 #################################################
 # Bash script - Modelo					#
@@ -7,19 +7,10 @@
 #################################################
 
 
-#############     CONFIG     ####################
-#Carrega variáreis de ambiente.
-source ~/.export
-# Suporte a cores no bash.
-source ~/shell/colors.sh
-################################################
-
 
 ##########  Funcao       #######################
 function help(){
-
     echo -en "$\n\n\t $alert Use: $green $0 $PARAMETERS $normal \n\n"
-
 }
 
 
@@ -41,7 +32,7 @@ case $1 in
 		* )
 			# Executa com opcao que nao tem.
 			export NAMESPACE=$1
-			echo -en "NAMESPACE=$red$1$normal\n"
+			echo -en "NAMESPACE=$red$1$green exported $normal\n\n"
 		;;
 esac
 #############        FIM      ##################
