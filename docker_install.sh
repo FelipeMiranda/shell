c9ccfdf06e064486403fed3202688dc0f12e6084
Rafael <quirinobytes@gmail.com>
2019-02-27 22:58:16 -0300

    OK

---
+++ docker_install.sh
@@ -0,0 +1,57 @@
+
+
+
+#!/bin/bash
+
+#################################################
+# Bash script - Modelo					#
+# Use como modelo para criar seus scripts bash. #
+# v_1.0.1							#
+#################################################
+VERSION=1.0
+
+
+#############     CONFIG     ####################
+#Carrega variáreis de ambiente.
+source ~/.export
+# Suporte a cores no bash.
+source ~/shell/colors.sh
+################################################
+
+
+##########  Funcao       #######################
+function help(){
+    echo -en "$\n\n\t $alert Use: $0 $green -i $normal \t Para instalar o docker versao 17.03.02\n"
+}
+
+function install(){
+	curl https://releases.rancher.com/install-docker/17.03.sh | sh
+}
+
+
+
+################################################
+#############        MAIN       ################
+case $1 in
+
+		"-i" )	
+			# Quando executa sem opcao, chama funcao versao acima.
+			install
+			exit
+		;;
+
+		"-h"| "--help" )	
+			# Quando executa sem opcao, chama funcao versao acima.
+			help
+			exit
+		;;
+
+
+		* )
+			# Executa com opcao que nao tem.
+			echo -en "O comando $red $1 $normal não existe\n\n\t Use $green $0 --help.$normal\n"
+		;;
+esac
+#############        FIM      ##################
+################################################
+
