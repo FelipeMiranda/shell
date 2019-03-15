c903d2a573a3776a72731845ac22b964ca8b0af2
Rafael Castro <quirinobytes@gmail.com>
2019-03-15 10:01:43 -0300

    Commiting my job!

--- push/cdshelld.js
+++ push/cdshelld.js
@@ -130,24 +130,18 @@ else{
    // Fazendo uma chamada de registro no servidorpush passando a versao, mainfunction e autoupdate(true/false).
     exec('/root/shell/linux/cdshell -V', (err, stdout, stderr) => {
 			const mainfunctionPath = '/root/.cdshell.mainfunction'
-			//se o arquivo existe, entao popule a variavel mainfunctino
+
+			fs.readFile( mainfunctionPath, function(err, data) {
 			if (!fs.existsSync(mainfunctionPath)) {
 				let mainfunction = 'default'
 			}
-			if (!fs.existsSync(mainfunctionPath)) {
-				let autoupdate = 'true'
-			}
 
-			fs.readFile( mainfunctionPath, function(err, data) {
-			mainfunction = data.toString();
-
-            hostversion = stdout;
-			hostversion.replace("\n","");
-			console.log(hostversion);
+			let autoupdate = 'true'
+			mainfunction = data.toString()
+            hostversion = stdout
 			console.log("Service CDSHELLD [STARTED] | Host[" + hostname + "] | v."+ hostversion )
-	        socket.emit('hostversion', {message : hostversion , hostname: hostname, hostconfig: {mainfunction:mainfunction,autoupdate:autoupdate}});
-
-			})
+	        socket.emit('hostversion', {message : hostversion , hostname: hostname, hostconfig: {mainfunction:mainfunction,autoupdate:autoupdate}})
+		})
     });
 
 
