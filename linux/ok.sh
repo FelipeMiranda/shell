1db732c4aa843a7159d69b85dd9bfa8eb10fc177
quirinobytes <quirinobytes@gmail.com>
2019-03-15 04:27:58 -0300

    colocando a ok.sh pra fuder

--- linux/ok.sh
+++ linux/ok.sh
@@ -32,7 +32,7 @@ function commit(){
 if [ $# -gt 1 ]; then
     hello $*
     git add .
-    git commit -m "$*" . | realce.awk "'$*'" 
+    git commit -m "$*" . | realce.awk <<< echo "'$*'" 
 #|| ERROR_CODE=1
     git push || ERROR_CODE=1
 fi
