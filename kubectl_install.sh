fa4b3b7ad347d00009c4af2a39d6c9797d7c6db4
Rafael <quirinobytes@gmail.com>
2019-03-15 01:49:17 -0300

    Commiting my job!

--- kubectl_install.sh
+++ kubectl_install.sh
@@ -33,21 +33,8 @@ if [ $FLAVOR == "Ubuntu" ]; then
     echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
     sudo apt-get update
     sudo apt-get install -y kubectl
+    exit
 fi
-
-if [ $FLAVOR == "Centos" ]; then
-    cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-    [kubernetes]
-    name=Kubernetes
-    baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-    enabled=1
-    gpgcheck=1
-    repo_gpgcheck=1
-    gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-    EOF
-    yum install -y kubectl
-fi
-
 
 
 }
