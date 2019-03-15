728a61970d943107a4a1709bed8b3665ec6c4db9
Rafael Castro <quirinobytes@gmail.com>
2019-03-14 22:47:38 -0300

    removendo o quebra de linha do cdshell -V

--- install.sh
+++ install.sh
@@ -215,9 +215,9 @@ MES=$(date +'%B')
 echo -en . 
 HORA=$(date +'%T')
 echo -en . 
-V=`git rev-list HEAD | wc -l `
+V=$(git rev-list HEAD | wc -l | tr -d '\n')
 echo -en . 
-VERSION=$(echo $V  | sed  's/./\0\./g; s/.$//')
+VERSION=$(echo -en $V  | sed  's/./\0\./g; s/.$//')
 HOST=$(hostname)
 # Montando a SAIDA para carimbar o arquivo.
 SAIDA="$alert ________________________________________________________________________ $normal \n"
@@ -230,7 +230,7 @@ SAIDA=$SAIDA"$blue#$normal  Server Hostname: $WHITE $HOST $normal \n"
 SAIDA=$SAIDA"$blue#$normal  Instalador: $REDALERT $0 $normal \n"
 SAIDA=$SAIDA"$blue##########################################################################$normal \n"
 echo $SAIDA > $BACKUP_DIR/data_ultima_instalacao.txt
-echo $VERSION > $BACKUP_DIR/versao_ultima_instalacao.txt
+echo $VERSION | head 1 > $BACKUP_DIR/versao_ultima_instalacao.txt
 echo -en ".......... $green Done $normal\n"
 
 # Instalacao do POST COMMIT do GIT HOOKS !
---
+++ linux/Getnodes.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#################################################
+# Bash script - Modelo					#
+# Use como modelo para criar seus scripts bash. #
+# v_1.0.1							#
+#################################################
+VERSION=1.0
+
+
+#############     CONFIG     ####################
+#Carrega variáreis de ambiente.
+source ~/.export
+# Suporte a cores no bash.
+source ~/shell/colors.sh
+################################################
+
+
+##########  Funcao       #######################
+function help(){
+    echo -en "$\n\n\t $alert Use: $green $0 $PARAMETERS $normal \n\n"
+}
+
+function versao(){
+    echo -en "$\n\n\t $alert Versao: $green $0 $VERSION $normal \n\n"
+}
+
+function exibeNodes(){
+ 	curl http://localhost:3000/rest/nodes | jq '.[] | (.hostname) + " => [" + (.version) + "]"'
+}
+
+################################################
+#############        MAIN       ################
+case $1 in
+		"ps" | "-p" | "--ps")	
+			# Faça isso... 
+			ps aux ; echo "Digite os comandos aqui"
+		;;
+
+
+		"" )	
+			# Quando executa sem opcao, chama funcao versao acima.
+			exibeNodes
+		;;
+
+		"-h"| "--help" )	
+			# Quando executa sem opcao, chama funcao versao acima.
+			help
+		;;
+
+
+		* )
+			# Executa com opcao que nao tem.
+			echo -en "Verificando os parametros passados $red $* $normal se existe algum.\n"
+			hasParams $*
+		;;
+esac
+#############        FIM      ##################
+################################################
+
