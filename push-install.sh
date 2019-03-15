9cb965cf79cad26e64da8fa82c5f38eead949432
Rafael <quirinobytes@gmail.com>
2019-03-14 23:54:54 -0300

    removendo o quebra de linha do push-install

--- push-install.sh
+++ push-install.sh
@@ -206,7 +206,7 @@ HORA=$(date +'%T')
 echo -en . 
 V=`git rev-list HEAD | wc -l | tr -d '\n' `
 echo -en . 
-VERSION=$(echo $V  | sed  's/./\0\./g; s/.$//')
+VERSION=$(echo -en $V | sed  's/./\0\./g; s/.$//')
 HOST=$(hostname)
 # Montando a SAIDA para carimbar o arquivo.
 SAIDA="$alert ________________________________________________________________________ $normal \n"
