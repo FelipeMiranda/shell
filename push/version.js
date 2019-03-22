349712919a1267bd5b60b8529817580882571721
quirinobytes <quirinobytes@gmail.com>
2019-03-21 21:49:58 -0300

    melhorias na version.js

--- push/version.js
+++ push/version.js
@@ -1,65 +1,61 @@
 #!/usr/bin/env node
 
-	var os = require("os")
-	const io = require("socket.io-client")
-	var socket = io.connect('http://servidorpush.superati.com.br:3000')
-	var fs = require('fs')
-	const { exec } = require('child_process')
-	var Promise = require("Bluebird")
+var os = require('os')
+const io = require('socket.io-client')
+var socket = io.connect('http://servidorpush.superati.com.br:3000')
+var fs = require('fs')
+const { exec } = require('child_process')
 
-		//buttons and inputs
-		var sistema = process.argv[2]
-		var hostname = os.hostname();
-		var hostversion = "";
+// Configuracao
+const mainfunctionPath = '/root/.cdshell.mainfunction'
+var mainfunction = 'default'
 
-	if ( sistema == "-h") {
-		console.log("\n\t ### version.js help ### \n")
-		console.log(" • Tente:  $>"+ process.argv[1] +" <OPCAO>\n")
-		console.log(" • OPCOES: ")
-		console.log("\t<REPOSITORIO>  ->   Use para saber a versao desse repositorio no servidor. ")
-		console.log("\t./version.js -h \t -> \t Mostra o Help \n")
-		console.log(" • Exemplo: ")
-		console.log("\t./version.js cdshell \t -> \t Mostra a versao do *cdshell* do servidorPush. ")
-		console.log("\t./version.js sistema \t -> \t Mostra a versao do *sistema* do servidorPush. ")
-		console.log("\t./version.js workspace \t -> \t Mostra a versao do *sistema* do servidorPush. ")
-		console.log("\n")
-		console.log("\t./version.js \t\t -> \t Faz o Registro da versao *desse* node no servidorPush. ")
-		console.log("\n")
-		process.exit(1);
-	}
+// caso o arquivo de mainfunction exista, pegar a funcao nele.
+if (fs.existsSync(mainfunctionPath)) {
+  mainfunction = fs.readFileSync(mainfunctionPath, 'utf8').toString().replace('\n', '')
+}
 
+console.log('Funcao=' + mainfunction.toString().replace('\n', ''))
 
-	//Emit a username
+// buttons and inputs
+var sistema = process.argv[2]
+var hostname = os.hostname()
+var hostversion = ''
 
-	exec("/root/shell/linux/cdshell -V", (err, stdout, stderr) => {
-		const mainfunctionPath = '/root/.cdshell.mainfunction'
-		var mainfunction = ''
-		if (!fs.existsSync(mainfunctionPath)) {
-			mainfunction = 'default'
-		}
-		else{
-			mainfunction = fs.readFile( mainfunctionPath, (err, data) => {	return data.toString().replace('\n','') })
-		}
-		console.log("mainfunction" + mainfunction)
-		hostversion = stdout;
-		hostversion.replace('\n','');
-		
-		socket.emit('hostversion', {message : hostversion , hostname: hostname, hostconfig: {'mainfunction': mainfunction , 'autoupdate':true} })
-	    //socket.emit('hostversion', {message : hostversion , hostname: hostname })
+if (sistema === '-h') {
+  console.log('\n\t ### version.js help ### \n')
+  console.log(' • Tente:  $>' + process.argv[1] + ' <OPCAO>\n')
+  console.log(' • OPCOES: ')
+  console.log('\t<REPOSITORIO>  ->   Use para saber a versao desse repositorio no servidor. ')
+  console.log('\t./version.js -h \t -> \t Mostra o Help \n')
+  console.log(' • Exemplo: ')
+  console.log('\t./version.js cdshell \t -> \t Mostra a versao do *cdshell* do servidorPush. ')
+  console.log('\t./version.js sistema \t -> \t Mostra a versao do *sistema* do servidorPush. ')
+  console.log('\t./version.js workspace \t -> \t Mostra a versao do *sistema* do servidorPush. ')
+  console.log('\n')
+  console.log('\t./version.js \t\t -> \t Faz o Registro da versao *desse* node no servidorPush. ')
+  console.log('\n')
+  process.exit(1)
+}
 
-		socket.emit('version', {message : sistema });
-		//socket.emit('sair', {message : "sair" });
-    })
+exec('/root/shell/linux/cdshell -V', (err, stdout, stderr) => {
+  console.log('Mainfunction=\"' + mainfunction + '"')
+  hostversion = stdout.toString().replace('\n', '')
 
-	socket.on("message", (data) => {
-		console.log(data.message);
-		socket.emit('sair', {message : "sair" })
-	})
+  // socket.emit('hostversion', {message : hostversion , hostname: hostname })
+  socket.emit('hostversion', { message: hostversion, hostname: hostname, hostconfig: { 'mainfunction': mainfunction, 'autoupdate': true } })
 
-	socket.on("sair", (data) => {
-    	socket.disconnect()
-	})
+  // socket.emit('sair', {message : 'sair' });
+  socket.emit('version', { message: sistema })
 
+  if (err) console.log(err)
+})
 
+socket.on('message', (data) => {
+  console.log(data.message)
+  socket.emit('sair', { message: 'sair' })
+})
 
-
+socket.on('sair', (data) => {
+  socket.disconnect()
+})
