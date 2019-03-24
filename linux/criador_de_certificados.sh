ebb14592c55c190855fe7711ec52295f4dfa64ce
quirinobytes <quirinobytes@gmail.com>
2019-03-24 12:09:05 -0300

    Novas melhorias no arquivo .alias

--- linux/criador_de_certificados.sh
+++ linux/criador_de_certificados.sh
@@ -7,7 +7,7 @@ echo -en "\n\t  $green ----------------------------- $normal \n\n"
 if [ -e ./certificados ]; then
  echo -en "\n\n\n$alert-> Ja existe uma pasta $normal $red './certificados' $alert aqui$normal\n"
  F="$(find ./certificados)"
- echo -en "\n\n$green$> ls $red ./certificados\n"
+ echo -en "\n$green$> ls $red ./certificados\n"
  echo -en "$cyan------------------------------$normal"
  echo -en "\n $magenta $F $normal \n"
  echo -en "$cyan------------------------------$normal\n"
@@ -28,9 +28,9 @@ if [ -e ./certificados ]; then
 fi
 
 echo -en "-> Criando os certificados na pasta $magenta./certificados $normal\n"
-echo -en "-> Deixar suas keys $magenta keys.pem $normal e $magenta cert.pem $normal >na pasta $red ./certificados/keys \n\n"
+echo -en "-> Deixar suas keys $magenta keys.pem $normal e $magenta cert.pem $normal na pasta -> $red ./certificados/keys \n\n"
 echo -en " $magenta [cert.pem] $normal  => $green certificados/keys/${magenta}cert.pem$normal\n"
-echo -en " $magenta [cert.pem] $normal  => $green certificados/keys/${magenta}keys.pem$normal\n"
+echo -en " $magenta [cert.pem] $normal  => $green certificados/keys/${magenta}keys.pem$normal\n\n\n.....................................\n"
 mkdir ./certificados/keys -p
 openssl req -new > certificados/cert.csr 
 openssl rsa -in privkey.pem -out certificados/keys/key.pem
@@ -38,7 +38,11 @@ openssl x509 -in certificados/cert.csr -out certificados/keys/cert.pem -req -sig
 if [ $? == 0 ]; then
       mkdir certificados/pem -p
       mv *.pem certificados/pem
-      echo -en "$green $alert Certificados criados com $normal $green sucesso ! $normal na pasta $magenta ['$(pwd)/certificados'] $normal \n"
+      echo -en "\n\n\n-> Certificados criados na pasta $magenta ['$(pwd)/certificados/keys'] $magenta \n"
+      echo -en "\n$green$> ls $red ./certificados/keys\n$cyan"
+      ls -la ./certificados/keys
+      echo -en "$normal"
+      echo -en "\n\n\n\t$green $alert Certificados criados com $normal $green sucesso !\n\n\n"
 else
       echo -en "$red Error: Erro ao gerar certificados: ['$(pwd)/certificados'] $normal \n"
 fi
